045a9928f4fe321a8b17baba0f9168d01055c405
linzhenan-mac <zhenan@linzhenan-macdeMacBook-Pro.local>
2016-12-22 16:59:48 +0800

    shell

---
+++ shell/config_ffmpeg_with_x264.sh
@@ -0,0 +1,1 @@
+./configure --enable-libx264 --enable-gpl --extra-cflags="-I/home/zhangjiejun/dev/x264" --extra-ldflags="-ldl -L/home/zhangjiejun/dev/x264" && make clean -j 8 && make -j 8
---
+++ shell/install_ffmpeg_with_opencv.sh
@@ -0,0 +1,2 @@
+cp ./ffmpeg.rb /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/ffmpeg.rb
+brew install ffmpeg --with-fdk-aac --with-libass --with-sdl2 --with-opus --with-opencv --with-x265 --with-webp
