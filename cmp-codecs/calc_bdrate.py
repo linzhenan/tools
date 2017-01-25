b9c0562b772e2d9506478eb988c2c2ac7362c8b8
linzhenan-mac <zhenan@linzhenan-macdeMacBook-Pro.local>
2017-01-25 21:09:44 +0800

    update calc_bdrate.py and add BDBR.txt

--- cmp-codecs/calc_bdrate.py
+++ cmp-codecs/calc_bdrate.py
@@ -40,37 +40,46 @@ results = {}
 current_video_name = 'invalid_video_name'
 currnet_encoder_name = 'invalid_encoder_name'
 
-with open ('run4.info') as f:
-    line = f.readline()
-    v = video()
-    e = encoder()
-    while line:
-        line = line.replace(" ","")
-        line = line.replace("\n","")
-        elems = line.split('\t')
-        print elems
-        if len(elems) == 1:
-            current_video_name = elems[0]
-            results[current_video_name] = {}
-        else:
-            current_encoder_name = elems[0]
-            if not results[current_video_name].has_key(current_encoder_name):
-                results[current_video_name][current_encoder_name] = encoder() 
-                results[current_video_name][current_encoder_name].set_encoder(elems[1], elems[0].split('_')[1], float(elems[3]))
-            results[current_video_name][current_encoder_name].add_record(elems[4], elems[5], elems[6], elems[7])
-        line = f.readline()
+selected_result_files = ['selected50.txt', 'selected50_qy265.txt', 'selected50_intel_medium.txt']
 
-print results['VID_20160725_091502.mp4']['x264_veryfast'].get_psnr_metric()
-print results['VID_20160725_091502.mp4']['x265_veryfast'].get_psnr_metric()
+for result_file in selected_result_files:
+    with open (result_file) as f:
+        line = f.readline()
+        while line:
+            line = line.replace(" ","")
+            line = line.replace("\n","")
+            elems = line.split('\t')
+            if len(elems) == 1:
+                current_video_name = elems[0]
+                if not results.has_key(current_video_name):
+                    results[current_video_name] = {}
+            else:
+                current_encoder_name = elems[0]
+                if not results[current_video_name].has_key(current_encoder_name):
+                    results[current_video_name][current_encoder_name] = encoder() 
+                    results[current_video_name][current_encoder_name].set_encoder(elems[1], elems[0].split('_')[1], float(elems[3]))
+                results[current_video_name][current_encoder_name].add_record(elems[4], elems[5], elems[6], elems[7])
+            line = f.readline()
 
 #the first metric_set is reference, and the second one is tested.
-print bdpsnr_limited.bdrate(results['VID_20160725_091502.mp4']['x264_veryfast'].get_psnr_metric(), results['VID_20160725_091502.mp4']['x265_veryfast'].get_psnr_metric())
+#print results['VID_20160725_091502.mp4']['x264_veryfast'].get_psnr_metric()
+#print results['VID_20160725_091502.mp4']['x265_veryfast'].get_psnr_metric()
+#print bdpsnr_limited.bdrate(results['VID_20160725_091502.mp4']['x264_veryfast'].get_psnr_metric(), results['VID_20160725_091502.mp4']['x265_veryfast'].get_psnr_metric())
 
-encoder_list = ['h264_qsv_veryfast', 'hevc_qsv_hw_veryfast', 'hevc_qsv_sw_veryfast', 'x264_veryfast', 'x265_veryfast']#, 'qy265_veryfast']
+tested_encoder_list = ['h264_qsv_veryfast', 'hevc_qsv_hw_veryfast', 'hevc_qsv_sw_veryfast', 'x265_veryfast', 'qy265_veryfast', 'hevc_qsv_hw_veryfast', 'hevc_qsv_sw_veryfast']
 
+print 'BDBR(PSNR)'
 for video_name, encoders in results.iteritems():
     line = video_name
-    for encoder_name in encoder_list:
+    for encoder_name in tested_encoder_list:
+        bdrate = bdpsnr_limited.bdrate(encoders['x264_veryfast'].get_psnr_metric(), encoders[encoder_name].get_psnr_metric())
+        line = line + ' ' + str(bdrate)
+    print line
+
+print 'BDBR(SSIM)'
+for video_name, encoders in results.iteritems():
+    line = video_name
+    for encoder_name in tested_encoder_list:
         bdrate = bdpsnr_limited.bdrate(encoders['x264_veryfast'].get_psnr_metric(), encoders[encoder_name].get_psnr_metric())
         line = line + ' ' + str(bdrate)
     print line
