0292b1dc70dd3973053456e40380880720aa1a69
Zhenan Lin <linzhenan@kuaishou.com>
2017-04-19 20:20:05 +0800

    fix bugs

--- aacbs/main.c
+++ aacbs/main.c
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
 {
     char *fi, *fo = NULL;
     FILE *pfi, *pfo = NULL;
-    int len = 0;
+    int len = 0, sync_pos = 0;
     int i = 0, j = 0;
     uint8_t *bs = NULL;
     uint32_t buf = 0;
@@ -82,21 +82,24 @@ int main(int argc, char** argv)
         // spec 3.13
         if ((buf & 0xFFF0) == 0xFFF0)
         {
-            buf <<= 8;
-            buf |= bs[i + 1];
-            buf <<= 8;
-            buf |= bs[i + 2];
-            i += 2;
-            header.id                           = (buf >> 19) & 0x01;
-            header.layer                        = (buf >> 17) & 0x03;
-            header.protection_absent            = (buf >> 16) & 0x01;
-            header.profile                      = (buf >> 14) & 0x02;
-            header.sampling_frequency_index     = (buf >> 10) & 0x0F;
-            header.private_bit                  = (buf >> 9) & 0x01;
-            header.channel_configuration        = (buf >> 6) & 0x02;
-            header.original_copy                = (buf >> 5) & 0x01;
-            header.home                         = (buf >> 4) & 0x01;
-            printf("syncword @ 0x%08X: %d %d %d %d %2d %d %d %d %d, ", i,
+            sync_pos = i - 1;
+            header.id                           = (buf >> 3) & 0x01;
+            header.layer                        = (buf >> 1) & 0x03;
+            header.protection_absent            = (buf >> 0) & 0x01;
+            for (j = 0; j < 4; j++)
+            {
+                i++;
+                buf <<= 8;
+                buf |= bs[i]; 
+            }
+            header.profile                      = (buf >> 30) & 0x02;
+            header.sampling_frequency_index     = (buf >> 26) & 0x0F;
+            header.private_bit                  = (buf >> 25) & 0x01;
+            header.channel_configuration        = (buf >> 22) & 0x07;
+            header.original_copy                = (buf >> 21) & 0x01;
+            header.home                         = (buf >> 20) & 0x01;
+            printf("syncword @ 0x%08X: %d %d %d %d %2d %d %d %d %d, ",
+                    sync_pos,
                     header.id,
                     header.layer,
                     header.protection_absent,
@@ -106,17 +109,16 @@ int main(int argc, char** argv)
                     header.channel_configuration,
                     header.original_copy,
                     header.home);
-            vheader.copyright_identification_bit        = (buf >> 3) & 0x01;
-            vheader.copyright_identification_start      = (buf >> 2) & 0x01;
-            for (j = 1; j <= 3; j++)
-            {
-                 buf <<= 8;
-                 buf |= bs[i + j];
-            }
-            i += 3;
-            vheader.frame_length                        = (vheader.frame_length << 11) | ((buf >> 13) & 0x1FFF);
-            vheader.adts_buffer_fullness                = (buf >> 2) & 0x1FFF;
-            vheader.number_of_raw_data_blocks_in_frame  = buf & 0x03;
+            vheader.copyright_identification_bit        = (buf >> 19) & 0x01;
+            vheader.copyright_identification_start      = (buf >> 18) & 0x01;
+            vheader.frame_length                        = (buf >> 5 ) & 0x1FFF;
+            vheader.adts_buffer_fullness                = (buf >> 0 ) & 0x1F;
+            i++;
+            buf <<= 8;
+            buf |= bs[i];
+            vheader.adts_buffer_fullness                <<= 6;
+            vheader.adts_buffer_fullness                = (buf >> 2 ) & 0x3F;
+            vheader.number_of_raw_data_blocks_in_frame  = (buf >> 0 ) & 0x03;
             printf("%d %d %5d %5d %d, ",
                     vheader.copyright_identification_bit,
                     vheader.copyright_identification_start,
@@ -127,21 +129,20 @@ int main(int argc, char** argv)
             {
                 for (j = 1; j <= vheader.number_of_raw_data_blocks_in_frame; j++)
                     i += 2;
-                crc = ((uint16_t)bs[i] << 8) | bs[i + 1];
+                i++;
+                crc = bs[i];
+                i++;
+                crc <<= 8;
+                crc |= bs[i];
                 printf("0x%04X", crc);
                 if (MODIFY_CRC)
                 {
-                    bs[i]       = (MODIFY_CRC >> 8) & 0x0F;
-                    bs[i + 1]   = MODIFY_CRC & 0xFF;
+                    bs[i - 1]   = (MODIFY_CRC >> 8) & 0xFF;
+                    bs[i - 0]   = MODIFY_CRC & 0xFF;
                     printf(" -> 0x%04X", MODIFY_CRC);
                 }
-                i += 2;
-                printf("\n"); 
             }
-            else
-            {
-                printf("\n");
-            }
+            printf("\n");
         }  
     }
     if (pfo)
