4984e17644c030d3e941d91f3c36290eb1ad043c
Zhenan Lin <linzhenan@kuaishou.com>
2017-05-02 21:05:56 +0800

    Add ADTS filled with PTS into AAC bitstream

--- aacbs/main.c
+++ aacbs/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <memory.h>
+#define MODIFY_CRC 0
 
-#define MODIFY_CRC 0x0101
+typedef enum tagSynEleId
+{
+    ID_SCE = 0,
+    ID_CPE,
+    ID_CCE,
+    ID_LFE,
+    ID_DSE,
+    ID_PCE,
+    ID_FIL,
+    ID_END,
+}
+stSynEleId;
 
 typedef struct tagAdtsFixedHeader
 {
@@ -28,6 +41,25 @@ typedef struct tagAdtsVariableHeader
 }
 stAdtsVariableHeader;
 
+#define enum2str(e) ""#e""
+
+char *id2str(uint8_t syn_ele_id)
+{
+    char *str = "";
+    switch (syn_ele_id)
+    {
+        case ID_SCE: str = enum2str(ID_SCE); break;
+        case ID_CPE: str = enum2str(ID_CPE); break;
+        case ID_CCE: str = enum2str(ID_CCE); break;
+        case ID_LFE: str = enum2str(ID_LFE); break;
+        case ID_DSE: str = enum2str(ID_DSE); break;
+        case ID_PCE: str = enum2str(ID_PCE); break;
+        case ID_FIL: str = enum2str(ID_FIL); break;
+        case ID_END: str = enum2str(ID_END); break;
+    }
+    return str;
+}
+
 int main(int argc, char** argv)
 {
     char *fi, *fo = NULL;
@@ -40,6 +72,32 @@ int main(int argc, char** argv)
     stAdtsVariableHeader vheader = { 0 };
     uint16_t crc = 0;
     uint8_t zero_left = 0;
+    int first_id = 0;
+    uint8_t fil[256] = { 0 };
+    uint8_t adts[256] = { 0 };
+    int write_len = 0;
+    int write_pos = 0;
+    /* 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 */
+    /* id    cnt<15  ex_type ........  */
+    fil[0] = ID_FIL << 5;   //id
+    fil[0] |= 6 << 1;       //count
+    fil[2] |= 0;            //pts
+    fil[3] |= 0;            //pts
+    fil[4] |= 0;            //pts
+    fil[5] |= 1;            //pts = 1
+    fil[6] |= 1;            //ID_END 1/3
+    fil[7] |= 3 << 6;       //ID_END 2/3
+    adts[0] = 0xFF;//syncword 8/12
+    adts[1] = 0xF0;//syncword 4/12
+    adts[1] |= 0 << 3;//id 1
+    adts[1] |= 0 << 1;//layer 2
+    adts[1] |= 1 << 0;//protection_absent 1
+    adts[2] = 0;//profile 2, sampling_frequency_index 4, private_bit 1, channel_configuration 1/3
+    adts[3] = 0;//channel_configuration 2/3, original_copy 1, home 1, copy_right_identification_bit 1, copyright_identification_start 1, frame_length 2/13
+    adts[4] = 0;//frame_length 8/13
+    adts[5] = 0;//frame_length 3/13, adts_buffer_fullness 5/11
+    adts[6] = 0;//adts_buffer_fullness 6/11, number_of_raw_data_blocks_in_frame 2
+    memcpy(adts + 7, fil, 8);//adts 15 bytes for pts
 
     if (argc < 2)
     {
@@ -82,6 +140,11 @@ int main(int argc, char** argv)
         // spec 3.13
         if ((buf & 0xFFF0) == 0xFFF0)
         {
+            fwrite(adts, 1, 15, pfo);
+            if (i - 1 - write_pos > 0)
+                fwrite(bs + write_pos, 1, i - 1 - write_pos, pfo);
+            write_pos = i - 1;
+
             sync_pos = i - 1;
             header.id                           = (buf >> 3) & 0x01;
             header.layer                        = (buf >> 1) & 0x03;
@@ -142,11 +205,25 @@ int main(int argc, char** argv)
                     printf(" -> 0x%04X", MODIFY_CRC);
                 }
             }
-            printf("\n");
-        }  
+            i++;
+            buf <<= 8;
+            buf |= bs[i];
+            first_id = (buf >> 5) & 0x07;
+            printf("                , %s\n", id2str(first_id));
+            /*
+            if (pfo)
+            {
+                fwrite(bs + write_pos, 1, i - write_pos, pfo);
+                write_len += i - write_pos;
+                write_pos = i;
+                fwrite(fil, 1, 6, pfo);
+                write_len += 6; 
+            }
+            */
+        }
     }
-    if (pfo)
-        fwrite(bs, 1, len, pfo);
+    //if (pfo)
+    //    fwrite(bs, 1, len, pfo);
 
 error:
     if (pfi) fclose(pfi);
